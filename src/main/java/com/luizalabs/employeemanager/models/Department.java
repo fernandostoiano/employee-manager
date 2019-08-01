d56cb18ac5c340c9154fef219e0360615252e9e4
fernando.gonzalez <fernando.stoiano@gmail.com>
2019-08-01 16:56:54 -0300

    Criated models and input new dependencies

---
+++ src/main/java/com/luizalabs/employeemanager/models/Department.java
@@ -0,0 +1,15 @@
+package com.luizalabs.employeemanager.models;
+
+public class Department {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/com/luizalabs/employeemanager/models/Employee.java
@@ -0,0 +1,35 @@
+package com.luizalabs.employeemanager.models;
+
+public class Employee {
+
+    private String name;
+
+    private String email;
+
+    private Department department;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+}
